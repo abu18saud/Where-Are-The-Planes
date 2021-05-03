af1bea446f10e81b6eafad800f42782e07965772
Abdullah Alsalem <abdullahsalem18@gmail.com>
2021-05-03 07:30:35 +0300

    Done
    Done

---
+++ Where_Are_The_Planes/Where_Are_The_Planes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Where_Are_The_Planes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WayStatus status = new WayStatus();
+        }
+        class Flight
+        {
+            public Flight()
+            {
+                Console.WriteLine("Registration");
+            }
+            ~Flight()
+            {
+                Console.WriteLine("Closed");
+            }
+        }
+        /*derive this class from Flight class,
+        define constructor and destructor for it*/
+        class WayStatus:Flight
+        {
+            public WayStatus()
+            {
+                Console.WriteLine("On the way");
+            }
+            ~WayStatus()
+            {
+                Console.WriteLine("Landed");
+            }
+        }
+    }
+}
